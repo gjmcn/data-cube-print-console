f916fc7c6ce8589c9e534f5dea4377f6312bf5b2
gmcn <gjmcn@users.noreply.github.com>
2018-04-17 17:50:15 +0100

    use tty-table

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# data-cube-print-shell
+Print [DataCubes](https://github.com/gjmcn/data-cube) in the Node.js console, browser and browser console.
+
+## Install/Load
+
+Uses the Universal Module Definition (UMD) so can be used in any JavaScript environment. In Node.js:
+
+* install:
+    ```
+    npm install --save data-cube-print-console
+    ```
+* load:
+   ```js
+   require('data-cube-print-console');
+   ```
+
+Currently, the only formatting option is whether to draw horizontal lines. Printed tables are 'compact' by default &mdash; lines are not drawn. To change this:
+
+```js
+const setOps = require('data-cube-print-console');
+setOps({compact: false});  //draw horizontal lines
+```
+
+## Use
+The package adds two methods to `Array.prototype`:
+
+`print`: print a cube (or a standard array)
+
+`info`: print summary information about a cube (or a standard array)
+
+If not passed an argument, the above methods print the relevant information and return the array/cube. This makes it easy to print intermediate results during a computation, e.g. `x.sum().print().log()`.
+
+If passed a truthy argument, `print` and `info` return the string to be printed, but do not print anything.
+
+## Notes
+
+* the default formatting makes it easy to indentify what things are (colors are not used in the browser):
+
+	* entries are colored based on type
+	* strings are quoted
+	* labels and keys are white; indices are gray and are wrapped in parentheses;
+
+* `print` and `info` do *not* convert a standard array to a cube like core DataCube methods
+
+* use [data-cube-print-html](https://github.com/gjmcn/data-cube-print-html) to print cubes as HTML tables
+
--- print-console.js
+++ print-console.js
@@ -1,75 +1,44 @@
 {
   'use strict'
   
-  const {table, getBorderCharacters} = require('table');
-  const colors = require('colors');
+  const Table = require('tty-table');
+  const chalk = require('chalk');
   const clog = console.log;
   
   
-  //---------- options ----------//
+  //--------------- options and prep ---------------//
   
   //high-level table options
   const dp = 4;
  	const maxPrint = 1e3;
+  const maxStr = 40
   const maxMag = 1e6;
-  
-  //default colors
-  const theme = {
-    number:    'yellow',
-    string:    'green',
-    boolean:   'magenta',
-    null:      'magenta',
-    undefined: 'magenta',
-    date:      'red',
-    other:     'cyan',
-    index:     'gray',
-    key:       'white',
-    label:     'white'
-  };
-  
-  //table ops for jsc
-  const getOpsJsc = nCol => {
-    const ops = { 
-      border: getBorderCharacters('void'),
-      columnDefault: { paddingLeft: 3, paddingRight: 1 },
-      drawHorizontalLine: () => { return false }
-    };
-    const cols = {};
-    for (let i=0; i<nCol; i++) cols[i] = {alignment: 'right'};
-    ops.columns = cols;
-    return ops;
-  };
-  
-  //table ops for jsci
-  const opsJsci = {
-    columns: { 0: { alignment: 'left' }, 1: { alignment: 'left' } },
-    border: getBorderCharacters('void'),
-    columnDefault: { paddingLeft: 3, paddingRight: 1 },
-    drawHorizontalLine: () => { return false }
-  };
-  
-  
-  //---------- prep ----------//
 
-  //check new string properties used by colors do not already exist
-  Object.keys(theme).map(a => {
-    if (a in String.prototype) {
-      throw new Error(a + ' is already a property of String.prototype')
-    }
-  });
-  colors.setTheme(theme);
+  //table ops for print
+  const printOps = {
+    borderStyle: (typeof window == 'object' ? 2 : 1),
+    borderColor : "gray",
+    headerAlign : "right",
+    paddingLeft: -6,
+    paddingRight: 1,
+    align : "right",
+    compact: true
+  };
   
   //check new array properties do not already exist
-  ['jsc','jsci'].map(a => {
+  ['print','info'].map(a => {
     if (a in Array.prototype) {
       throw new Error(a + ' is already a property of Array.protoype');
     }
   });
 
   
-  //---------- auxiliary ----------//
+  //--------------- auxiliary ---------------//
 
   const expand = {r: 'row', c: 'column', p: 'page'};
+  
+  //truncate string if longer than max
+  const truncate = (s,mx) => s.length > mx ? s.slice(0,mx) + ' ...' : s;
   
   //shape of cube as a comma-sep list
   const shapeStr = x => ['r','c','p'].map(a => x._d_c_[a]).join(', ')
@@ -77,29 +46,28 @@
   //entry format functions
   let fmt = {
     number: x => {
-      if (Number.isInteger(x)) return ('' + x).number;
+      let str;
+      if (Number.isInteger(x)) str = '' + x;
       let prec = Math.max(1,dp);
       if (x !== 0 && (Math.abs(x) >= maxMag || Math.abs(x) < Math.pow(10,-prec))) {
-        return (+x.toPrecision(prec)).toExponential().number;
+        str =  (+x.toPrecision(prec)).toExponential();
       }
-      return x.toFixed(dp).number;
+      str = x.toFixed(dp);
+      return chalk.yellow(str);
     },
-    string: x => {
-      if (x.length === 0) return '<empty string>'.string;
-      let maxLen = 25;
-      return(x.length > maxLen ? x.slice(0,maxLen) + ' ...' : x).string;
-    },
-    boolean: x => ('' + x).boolean,
-    null: () => 'null'.null,
-    undefined: () => 'undefined'.undefined,
-    date: x => x.toUTCString().date,
-    function: () => 'function'.other,
-    cube: x => ('cube (entries: ' + x.length + ', shape: ' + shapeStr(x) + ')').other,
-    array: x => ('array (entries: ' + x.length +')').other,
-    other: x => ('' + x).other,
-    index: x => ('' + x).index,
-    key: x => x.key,
-    label: x => x.label 
+    string: x => chalk.green(`'${truncate(x,maxStr)}'`),
+    boolean: x => chalk.magenta(x),
+    null: () => chalk.red('null'),
+    undefined: () => chalk.red('undefined'),
+    function: () => chalk.cyan('function'),
+    date: x => chalk.cyan(('' + x).replace(/\s*\([^)]*\)/, '')),
+    cube: x => chalk.cyan('cube (entries: ' + x.length + ', shape: ' + shapeStr(x) + ')'),
+    array: x => chalk.cyan('array (entries: ' + x.length +')'),
+    other: x => chalk.cyan(truncate('' + x, maxStr)),
+    index: x => chalk.gray('(' + x + ')'),
+    key: x => chalk.white(x),
+    label: x => chalk.white(x),
+    faint: x => chalk.gray(x)
   };
 
   //fmt entry
@@ -114,23 +82,24 @@
   };
 
   
-  //---------- print cube or standard array ----------//
-  
-  Array.prototype.jsc = function(ret) {
+  //--------------- print cube or standard array ---------------//
+
+  Array.prototype.print = function(retStr) {
+        
+    let str = '';
     
     //cube
     if (this._d_c_)  {
       if (this.length > maxPrint) {
         clog(`\n   (number of entries > ${maxPrint})`);
-        this.jsci();
+        this.info();
       }
-      else if (this.length === 0) this.jsci();
+      else if (this.length === 0) this.info();
       else {
         clog('');
         const dc = this._d_c_;
         const nr = dc.r, nc = dc.c, np = dc.p;
         const e = dc.e;
-        const ops = getOpsJsc(nc+1);
         let rowKey, pageKey, pageLabel;
         if (e) {
           if (e.rk) rowKey = true;
@@ -138,94 +107,93 @@
           if (e.pl) pageLabel= true;
         }
         let colInfo = new Array(nc+1);
-        
         //topLeft (col and row label) and col indices/keys
         if (e) {
-          //let topLeft = ''
-          //if (e.cl) topLeft += fmt.label(e.cl) + '▶';
-          //if (e.rl) {
-          //if (e.cl) topLeft += '---';
-          //  topLeft += fmt.label(e.rl) + '▼'
-          //}
-          //          if (e.ck) colInfo = [topLeft].concat(e.ca.map(fmt.key));
-          if (e.ck) colInfo = [''].concat(e.ca.map(fmt.key));
+          let topLeft = ''
+          if (e.cl) topLeft += fmt.label(e.cl + ' →');
+          if (e.rl) {
+            if (e.cl) topLeft += ' \n';
+            topLeft += fmt.label(e.rl + ' ↓');
+          }
+          if (e.ck) colInfo = [topLeft].concat(e.ca.map(fmt.key));
           else {
-            colInfo[0] = ''; //topLeft;
+            colInfo[0] = topLeft;
             for (let c=0; c<nc; c++) colInfo[c+1] = fmt.index(c);
           }
-          if (e.cl) colInfo[1] = `${fmt.label(e.cl)}:  ${colInfo[1]}`; 
         }
         else {
           colInfo[0] = '';
           for (let c=0; c<nc; c++) colInfo[c+1] = fmt.index(c);         
         }
-        
+        colInfo = colInfo.map(a => ({value:a})) //, paddingLeft: 1, paddingRight: 1}));   //???????????????inefficient? - even need the padding?
         //print pages
         for (let p=0; p<np; p++) {
           if (np > 1 || pageKey || pageLabel) {
-            clog(`   [${(pageLabel ? fmt.label(e.pl) : fmt.index('page')) + ':'} ${
-                       pageKey ? fmt.key(e.pa[p]) : fmt.index(p)}]`);
+            str += `  ${(pageLabel ? fmt.label(e.pl) : fmt.faint('page')) + fmt.faint(':')} ${
+                       pageKey ? fmt.key(e.pa[p]) : fmt.index(p)}`;
           }
-          
-          -POSS REDUCE PAGE INFO INDENT
-          -MAKE COLOR OF ':' CONSISTENT
-          
-          
-          let data = new Array(nr+1);
-          data[0] = colInfo;
+          let data = new Array(nr);
           for (let r=0; r<nr; r++) {
             let thisRow = new Array(nc+1);
             thisRow[0] = rowKey ? fmt.key(e.ra[r]) : fmt.index(r);
             for (let c=0; c<nc; c++) {
               thisRow[c+1] = fmtEntry(this[r + nr*c + nr*nc*p]); 
             }
-            data[r+1] = thisRow;
+            data[r] = thisRow;
           }
-          if (e && e.rl) data[1][0] = `${fmt.label(e.rl)}:  ${data[1][0]}`;
-          clog(table(data,ops));
+          str += Table(colInfo, data, printOps).render() + '\n';
         }
       }
+      str = str.slice((str[0] === '\n' ? 1 : 0), -1);  //remove \n from both ends of string
     }
-      
     //standard array
+    else str = fmt.faint('\n  (standard array)') + 
+      Table([], this.map( (a,i) => [fmt.index(i), fmtEntry(a)]), {...printOps}).render();
+    //print or return string
+    if (retStr) return str;
     else {
-      clog('\n   standard array, default printing:\n')
-      clog(this);
+      clog(str);
+      return this;
     }
   
-    return arguments.length > 0 ? arg : this;
   };
   
   
-  //---------- print info on cube or standard array ----------//
+  //--------------- print info on cube or standard array ---------------//
   
-  Array.prototype.jsci = function(ret) {
+  Array.prototype.info = function(retStr) {
+    let str;
     if (this._d_c_) {
       const dc = x._d_c_;
-      const y = [
-        ['entries:', x.length],
-        ['shape:', shapeStr(this)]
-      ];
       const keys = [];
       const labels = [];
       if (dc.e) {
         ['rk', 'ck' ,'pk'].map(a => {
-          if (dc.e[a]) keys.push(expand(a[0]));               
+          if (dc.e[a]) keys.push(expand[a[0]]);               
         }); 
         ['rl', 'cl' ,'pl'].map(a => {
-          if (dc.e[a]) labels.push(expand(a[0]));               
+          if (dc.e[a]) labels.push(expand[a[0]]);               
         });
       }
-      y.push(['keys:'  , keys.length   ? keys.join(', ')   : '--']);
-      y.push(['labels:', labels.length ? labels.join(', ') : '--']);    
-      clog('');
-      clog(table(y,opsJsci));
+      str = 
+        '\n  entries: ' + x.length + 
+        '\n  shape:   ' + shapeStr(this) +
+        '\n  keys:    ' + (keys.length ? keys.join(', ')   : '--') + 
+        '\n  labels:  ' + (labels.length ? labels.join(', ') : '--');
     }
-    else clog('\n   standard array, entries: ' + this.length);
-    return arguments.length > 0 ? arg : this;
+    else str = '\n  standard array\n  entries: ' + this.length;
+    if (retStr) return str;
+    else {
+      clog(str);
+      return this;
+    }
   };
   
-  //exported function can overwrite the format functions
-  module.exports = newFmt => fmt = {...fmt, ...newFmt};
-   
+  
+  //--------------- export function to set compact ---------------//
+  
+  module.exports = ops => {
+    if ('compact' in ops) printOps.compact = ops.compact;
+  }
+     
 }
